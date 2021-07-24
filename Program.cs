bc016e06efbff7fa19d8eecf0610fbe43dd1f35c
name <varunbrahmane30@gmail.com>
2021-07-24 16:58:14 +0530

    [varun] added: Mobile number input validation

--- Program.cs
+++ Program.cs
@@ -7,19 +7,24 @@ namespace Day19_RejexUserRegistration
         static void Main(string[] args)
         {
             
-            Console.Write("Enter first name:");
+            Console.Write("Enter first name : ");
             String fname = Console.ReadLine();
             Console.WriteLine(UserRegistration.validateFirstName(fname));
             Console.WriteLine();
 
-            Console.Write("Enter Last name:");
+            Console.Write("Enter Last name : ");
             String lname = Console.ReadLine();
             Console.WriteLine(UserRegistration.validateLastName(lname));
             Console.WriteLine();
 
-            Console.Write("Enter Eamil:");
+            Console.Write("Enter Eamil : ");
             String email = Console.ReadLine();
             Console.WriteLine(UserRegistration.validateEmail(email));
+            Console.WriteLine();
+
+            Console.Write("Enter Mobile Number : ");
+            String mobileNo = Console.ReadLine();
+            Console.WriteLine(UserRegistration.validateMobileNo(mobileNo));
             Console.WriteLine();
 
             Console.ReadKey();
