1f5df07a4795ca0aff70c71b7c0c53979ddfc41e
name <varunbrahmane30@gmail.com>
2021-07-24 16:39:21 +0530

    [varun] added: last name User input validation

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -9,6 +9,7 @@ namespace Day19_RejexUserRegistration
     {
        
         public static string Regex_Name = "^[A-Z]{1}[A-Za-z]{1}[a-zA-Z]*$";
+        public static string Regex_lName = "^[A-Z]{1}[A-Za-z]{1}[a-zA-Z]*$";
 
         public static String validateFirstName(string firstName)
         {
@@ -16,7 +17,15 @@ namespace Day19_RejexUserRegistration
                 return "First name is Valid";
             else
                 return "First name is InValid";
-        }   
+        }
+
+        public static String validateLastName(string lastName)
+        {
+            if (Regex.IsMatch(lastName, Regex_lName) == true)
+                return "last name is Valid";
+            else
+                return "last name is InValid";
+        }
     }
 
 }
