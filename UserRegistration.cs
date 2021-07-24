1f245000954b1a2d3b5fb4b103d31d5a22e34d72
name <varunbrahmane30@gmail.com>
2021-07-24 16:58:40 +0530

    [varun] added: mobile number input validation

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -11,8 +11,9 @@ namespace Day19_RejexUserRegistration
         public static string Regex_Name = "^[A-Z]{1}[A-Za-z]{1}[a-zA-Z]*$";
         public static string Regex_lName = "^[A-Z]{1}[A-Za-z]{1}[a-zA-Z]*$";
         public static string Regex_Email = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)*[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
+        public static string Regex_MobileNo = "(0|91)?[ ][6-9][0-9]{9}";
 
-        public static String validateFirstName(string firstName)
+    public static String validateFirstName(string firstName)
         {
             if (Regex.IsMatch(firstName, Regex_Name) == true)
                 return "First name is Valid";
@@ -34,6 +35,14 @@ namespace Day19_RejexUserRegistration
                 return "Email is Valid";
             else
                 return "Email is InValid";
+        }
+
+        public static String validateMobileNo(string mobileNo)
+        {
+            if (Regex.IsMatch(mobileNo, Regex_MobileNo) == true)
+                return "Mobile Number is Valid";
+            else
+                return "Mobile Number is InValid";
         }
     }
 
